4e14ea5db9c39319d3dd3602957c2ccf59ded230
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-03 19:46:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// const endpoint = "https://post-rest-api-default-rtdb.firebaseio.com";
-const endpoint = "";
+const endpoint = "https://post-rest-api-default-rtdb.firebaseio.com";
+// const endpoint = "";
 let posts;
 
 window.addEventListener("load", initApp);
